d8714e8a32d24973c73c5bc2bd6e3dc7c495665c
gabeaster <gab.easter@live.com>
2020-03-16 14:24:00 -0400

    step 1 - import the useState hook complete; moving on to step 2- establishing app's state with useState hooks.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-//TODO: STEP 1 - Import the useState hook.
-import React from "react";
+//TODO: STEP 1 - Import the useState hook. DONE
+import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
